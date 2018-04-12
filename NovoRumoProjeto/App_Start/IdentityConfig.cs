5e48124b2a6c18c7b330b19ac96988f455c7924b
Victor Oliveira <fontiana@gmail.com>
2018-04-12 17:26:07 -0300

    adicionando identity

---
+++ NovoRumoProjeto/App_Start/IdentityConfig.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using NovoRumoProjeto.Models;
+using System.Configuration;
+
+namespace NovoRumoProjeto
+{
+    public class ApplicationUserManager : UserManager<ApplicationUser, int>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser, int> store)
+            : base(store)
+        {
+        }
+
+        public static ApplicationUserManager Create(
+            IdentityFactoryOptions<ApplicationUserManager> options,
+            IOwinContext context)
+        {
+            var manager = new ApplicationUserManager(
+                new ApplicationUserStore(context.Get<ApplicationDbContext>()));
+
+            manager.UserValidator = new UserValidator<ApplicationUser, int>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+            
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
+            };
+            
+            manager.UserLockoutEnabledByDefault = true;
+            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+            
+            manager.RegisterTwoFactorProvider("PhoneCode",
+                new PhoneNumberTokenProvider<ApplicationUser, int>
+                {
+                    MessageFormat = "Your security code is: {0}"
+                });
+            
+            manager.RegisterTwoFactorProvider("EmailCode",
+                new EmailTokenProvider<ApplicationUser, int>
+                {
+                    Subject = "SecurityCode",
+                    BodyFormat = "Your security code is {0}"
+                });
+
+            manager.EmailService = new EmailService();
+            manager.SmsService = new SmsService();
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                manager.UserTokenProvider =
+                    new DataProtectorTokenProvider<ApplicationUser, int>(
+                        dataProtectionProvider.Create("ASP.NET Identity"));
+            }
+
+            var name = ConfigurationManager.AppSettings["AdminEmail"];
+            var user = manager.FindByName(name);
+            if (user == null)
+            {
+                var password = ConfigurationManager.AppSettings["AdminPassword"];
+                var role = ConfigurationManager.AppSettings["RoleName"];
+
+                user = new ApplicationUser { UserName = name, Email = name };
+                var result = manager.Create(user, password);
+                manager.AddToRole(user.Id, role);
+                result = manager.SetLockoutEnabled(user.Id, false);
+            }
+
+            return manager;
+        }
+    }
+
+    
+    public class ApplicationRoleManager : RoleManager<ApplicationRole, int>
+    {
+        public ApplicationRoleManager(IRoleStore<ApplicationRole, int> roleStore)
+            : base(roleStore)
+        {
+        }
+        
+        public static ApplicationRoleManager Create(
+            IdentityFactoryOptions<ApplicationRoleManager> options, IOwinContext context)
+        {
+            var roleManager = new ApplicationRoleManager(new ApplicationRoleStore(context.Get<ApplicationDbContext>()));
+
+            var role = roleManager.FindByName("Admin");
+            if (role == null)
+            {
+                role = new ApplicationRole("Admin");
+                role.Description = "Administrator role";
+                var roleresult = roleManager.Create(role);
+            }
+
+            role = roleManager.FindByName("Customer");
+            if (role == null)
+            {
+                role = new ApplicationRole("Customer");
+                role.Description = "Customer role";
+                var roleresult = roleManager.Create(role);
+            }
+
+            return roleManager;
+        }
+    }
+
+
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            //var mailObject = new MailObject();
+            //mailObject.Message = message.Body;
+            //mailObject.Subject = message.Subject;
+            //mailObject.Email = message.Destination;
+            //Mailer.SendRegularEmail(mailObject);
+            return Task.FromResult(0);
+        }
+
+        public void Send(IdentityMessage message)
+        {
+            //var mailObject = new MailObject();
+            //mailObject.Message = message.Body;
+            //mailObject.Subject = message.Subject;
+            //mailObject.Email = message.Destination;
+            //Mailer.SendRegularEmail(mailObject);
+        }
+    }
+
+
+    public class SmsService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your sms service here to send a text message.
+            return Task.FromResult(0);
+        }
+    }
+    
+    public class ApplicationDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            InitializeIdentityForEF(context);
+            base.Seed(context);
+        }
+
+        //Create User=Admin@Admin.com with password=Admin@123456 in the Admin role        
+        public static void InitializeIdentityForEF(ApplicationDbContext db)
+        {
+            var userManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var roleManager = HttpContext.Current.GetOwinContext().Get<ApplicationRoleManager>();
+
+            const string name = "admin@example.com";
+            const string password = "Admin@123456";
+            const string roleName = "Admin";
+
+            //Create Role Admin if it does not exist
+            var role = roleManager.FindByName(roleName);
+            if (role == null)
+            {
+                role = new ApplicationRole(roleName);
+                var roleresult = roleManager.Create(role);
+            }
+
+            var user = userManager.FindByName(name);
+            if (user == null)
+            {
+                user = new ApplicationUser { UserName = name, Email = name };
+                var result = userManager.Create(user, password);
+                result = userManager.SetLockoutEnabled(user.Id, false);
+            }
+
+            // Add user admin to Role Admin if not already added
+            var rolesForUser = userManager.GetRoles(user.Id);
+            if (!rolesForUser.Contains(role.Name))
+            {
+                var result = userManager.AddToRole(user.Id, role.Name);
+            }
+        }
+    }
+
+
+    public class ApplicationSignInManager : SignInManager<ApplicationUser, int>
+    {
+        public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager) :
+            base(userManager, authenticationManager)
+        { }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+        {
+            return user.GenerateUserIdentityAsync((ApplicationUserManager)UserManager);
+        }
+
+        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
+        {
+            return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
+        }
+    }
+}
---
+++ NovoRumoProjeto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NovoRumoProjeto.Startup))]
+namespace NovoRumoProjeto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
