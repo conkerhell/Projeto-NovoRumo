2f6b08eecc4e851fbf60a6a2d55cd38daee3df06
Victor <fontiana@gmail.com>
2018-04-17 09:11:50 -0300

    Adicionado projetos

---
+++ NovoRumoProjeto.Utilities/EmailCreator/EmailCreator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovoRumoProjeto.Utilities.EmailCreator
+{
+    public class EmailCreator : IEmailCreator
+    {
+        private MailMessage mailMessage = new MailMessage();
+
+        public IEmailCreator BCC(params string[] bccAddresses)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEmailCreator CC(params string[] ccAddresses)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEmailCreator From(string fromAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Send()
+        {
+            SmtpClient smtpClient = new SmtpClient();
+            //smtpClient.Credentials = cre
+            smtpClient.Timeout = 100;
+            smtpClient.Send(mailMessage);
+        }
+
+        public IEmailCreator To(params string[] toAddresses)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEmailCreator WithBody(string body)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEmailCreator WithSubject(string subject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NovoRumoProjeto.Utilities/EmailCreator/IEmailCreator.cs
@@ -0,0 +1,14 @@
+﻿
+namespace NovoRumoProjeto.Utilities.EmailCreator
+{
+    public interface IEmailCreator
+    {
+        IEmailCreator From(string fromAddress);
+        IEmailCreator To(params string[] toAddresses);
+        IEmailCreator CC(params string[] ccAddresses);
+        IEmailCreator BCC(params string[] bccAddresses);
+        IEmailCreator WithSubject(string subject);
+        IEmailCreator WithBody(string body);
+        void Send();
+    }
+}
