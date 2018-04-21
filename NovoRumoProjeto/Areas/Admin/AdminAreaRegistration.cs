b021032cee1dbc50f7e3a504d6334251114ecdf1
Victor Oliveira <fontiana@gmail.com>
2018-04-21 16:38:50 -0300

    Subindo area administrativa

---
+++ NovoRumoProjeto/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace NovoRumoProjeto.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                name: "Admin",
+                url: "Admin/{controller}/{action}/{id}",
+                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
