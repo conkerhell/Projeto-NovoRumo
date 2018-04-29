4f66cb80a520dce21eb958a91572fc77b47c4ec0
conkerhell <conker_hell@hotmail.com>
2018-04-29 14:31:17 -0300

    Add Day

---
+++ NovoRumoProjeto/Areas/Admin/Controllers/DiaController.cs
@@ -0,0 +1,28 @@
+﻿using NovoRumoProjeto.Areas.Admin.Models;
+using NovoRumoProjeto.Utilities;
+using System.Web.Mvc;
+namespace NovoRumoProjeto.Areas.Admin.Controllers
+{
+    public class DiaController: Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            var model = new DiaViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(DiaViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ NovoRumoProjeto/Areas/Admin/Models/DiaViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+using Resources;
+using System.ComponentModel.DataAnnotations;
+
+namespace NovoRumoProjeto.Areas.Admin.Models
+{
+    public class DiaViewModel
+    {
+
+        [Required(ErrorMessageResourceType = typeof(LocalizedMessages),
+            ErrorMessageResourceName = "PropertyValueRequired")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+    }
+}
