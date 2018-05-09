3b2ec86de980e7baca71bfa4b2e7fc59502e0f3c
Victor <fontiana@gmail.com>
2018-05-09 09:14:53 -0300

    Adding new Events features

---
+++ NovoRumoProjeto/Areas/Admin/Controllers/EventsController.cs
@@ -0,0 +1,50 @@
+﻿using NovoRumoProjeto.Areas.Admin.Models;
+using NovoRumoProjeto.Utilities;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace NovoRumoProjeto.Areas.Admin.Controllers
+{
+    [Authorize(Roles = Consts.ADMIN_ROLE)]
+    public class EventsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var model = new List<EventViewModel>();
+
+            model.Add(new EventViewModel()
+            {
+                Title = "Bazar beneficiente",
+                Description = "Lorem ipsum dolores"
+            });
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(EventViewModel model)
+        {
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            var model = new EventViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EventViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ NovoRumoProjeto/Areas/Admin/Models/EventViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace NovoRumoProjeto.Areas.Admin.Models
+{
+    public class EventViewModel
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Data { get; set; }
+        public string Endereço { get; set; }
+    }
+}
