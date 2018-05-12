71e1a795b70bc2edf1ed9254a950029c12fd4564
Victor Oliveira <fontiana@gmail.com>
2018-05-12 14:06:04 -0300

    Adding SQL Commands for Event entity

---
+++ NovoRumoProjeto.Entity/EventEntity.cs
@@ -0,0 +1,12 @@
+﻿
+namespace NovoRumoProjeto.Entity
+{
+    public class EventEntity
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Data { get; set; }
+        public string Endereco { get; set; }
+    }
+}
---
+++ Proc/Events/INIT_EVENTS.sql
@@ -0,0 +1,88 @@
+USE [NovoRumo]
+GO
+
+/****** Object:  Table [dbo].[Event]    Script Date: 5/12/2018 1:53:18 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Event](
+	[EventID] [int] IDENTITY(1,1) NOT NULL,
+	[Title] [varchar](200) NOT NULL,
+	[Description] [varchar](1000) NOT NULL,
+	[Data] [datetime] NOT NULL,
+	[Endereco] [varchar](200) NOT NULL,
+ CONSTRAINT [PK_Event] PRIMARY KEY CLUSTERED 
+(
+	[EventID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE spGetEvents
+AS
+BEGIN
+SELECT EventID, Title, Description, Data, Endereco FROM Event WITH (NOLOCK)
+END
+
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE spGetEventById (
+	@EventID AS INT
+)
+AS
+BEGIN
+SELECT EventID, Title, Description, Data, Endereco 
+  FROM Event WITH (NOLOCK)
+ WHERE EventID = @EventID
+END
+
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE spInsertEvent(
+	@Title AS VARCHAR(200),
+	@Description AS VARCHAR(1000),
+	@Data AS DATETIME,
+	@Endereco AS VARCHAR(200)
+)
+AS
+BEGIN
+INSERT INTO Event (
+		Title,
+		Description,
+		Data,
+		Endereco
+	) VALUES (
+		@Title,
+		@Description,
+		@Data,
+		@Endereco
+	)
+END
+
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
