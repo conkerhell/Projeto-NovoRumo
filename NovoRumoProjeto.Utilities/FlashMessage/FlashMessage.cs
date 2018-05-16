35bc4d544eb12f24da33f7ad4c566bf6185527e6
Victor <fontiana@gmail.com>
2018-05-16 18:23:02 -0300

    Adding new flashMessage

--- NovoRumoProjeto.Utilities/FlashMessage/FlashMessage.cs
+++ NovoRumoProjeto.Utilities/FlashMessage/FlashMessage.cs
@@ -3,5 +3,11 @@ namespace NovoRumoProjeto.Utilities.FlashMessage
 {
     public class FlashMessage
     {
+        private FlashMessage flashMessage = new FlashMessage();
+
+        public static void Success(string message)
+        {
+            //return 
+        }
     }
 }
---
+++ NovoRumoProjeto.Utilities/FlashMessage/FlashMessageTransport.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Web;
+
+namespace NovoRumoProjeto.Utilities.FlashMessage
+{
+    public class FlashMessageTransport
+    {
+        public string KeyName { get; set; }
+
+        public FlashMessageTransport()
+        {
+
+            KeyName = "_FlashMessage";
+        }
+
+        public void Add(FlashMessageModel messages)
+        {
+            // Set the data without doing any further securing or transformations.
+            var context = new HttpContextWrapper(HttpContext.Current);
+            //context.Session[KeyName] = data;
+        }
+    }
+}
