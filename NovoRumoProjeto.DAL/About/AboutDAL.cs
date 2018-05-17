9f5165a0ad6ed93060353c593571d2c985c0186c
conkerhell <conker_hell@hotmail.com>
2018-05-17 18:56:58 -0300

    Fix About

--- NovoRumoProjeto.DAL/About/AboutDAL.cs
+++ NovoRumoProjeto.DAL/About/AboutDAL.cs
@@ -9,7 +9,7 @@ namespace NovoRumoProjeto.DAL.About
     public class AboutDAL: DAL, IAboutDAL
     {
         private const string INSERT_ABOUT_PROC = "spInsertAbout";
-        private const string GET_ABOUT_PROC = "spGetDaily";
+        private const string GET_ABOUT_PROC = "spGetAbout";
         private const string GET_ABOUT_BY_ID_PROC = "spGetAboutByID";
         private const string DELETE_ABOUT_PROC = "spDeleteAbout";
         private const string UPDATE_ABOUT_PROC = "spUpdateAbout";
