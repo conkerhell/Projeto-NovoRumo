e1adbe61c727ea91418e0eec9d2a60474f5cd7b2
Victor Oliveira <fontiana@gmail.com>
2018-05-28 22:15:47 -0300

    PaymentCreator

--- NovoRumoProjeto.Entity/DonationEntity.cs
+++ NovoRumoProjeto.Entity/DonationEntity.cs
@@ -4,6 +4,7 @@ namespace NovoRumoProjeto.Entity
     public class DonationEntity
     {
         public int DonationID { get; set; }
+        public int OrderID { get; set; }
         public byte Status { get; set; }
     }
 }
--- NovoRumoProjeto.Utilities/Payment/IPaymentCreator.cs
+++ NovoRumoProjeto.Utilities/Payment/IPaymentCreator.cs
@@ -1,11 +1,11 @@
-﻿
+﻿using NovoRumoProjeto.Entity;
 using NovoRumoProjeto.Utilities.Payment.Model;
 
 namespace NovoRumoProjeto.Utilities.Payment
 {
     public interface IPaymentCreator
     {
-        //PaymentStatusIndicator Create(OrderEntity entity);
+        PaymentStatusIndicator Create(OrderEntity entity);
         bool CheckTransaction(string code, string id);
 
     }
