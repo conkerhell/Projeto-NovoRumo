982b98b1da5a6adc87faf1d6b5763995827f2d97
Victor <fontiana@gmail.com>
2018-06-19 09:30:57 -0300

    Adding INIT PROCs for Order

---
+++ Proc/Order/INIT_ORDER.sql
@@ -0,0 +1,96 @@
+USE [NovoRumo]
+GO
+
+
+/****** Object:  Table [dbo].[Type]    Script Date: 6/19/2018 9:25:11 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Type](
+	[TypeId] [int] NOT NULL,
+	[Name] [varchar](200) NOT NULL,
+	[Description] [varchar](500) NULL,
+ CONSTRAINT [PK_Type] PRIMARY KEY CLUSTERED 
+(
+	[TypeId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+/****** Object:  Table [dbo].[Order]    Script Date: 6/19/2018 9:24:17 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Order](
+	[OrderId] [int] NOT NULL,
+	[TypeId] [int] NOT NULL,
+	[UserId] [int] NOT NULL,
+	[NotificationCode] [varchar](50) NULL,
+	[PaypalGuid] [varchar](50) NULL,
+	[Total] [decimal](18, 2) NOT NULL,
+	[RecordDate] [date] NOT NULL,
+ CONSTRAINT [PK_Order] PRIMARY KEY CLUSTERED 
+(
+	[OrderId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[Order]  WITH CHECK ADD  CONSTRAINT [FK_Order_Type] FOREIGN KEY([TypeId])
+REFERENCES [dbo].[Type] ([TypeId])
+GO
+
+ALTER TABLE [dbo].[Order] CHECK CONSTRAINT [FK_Order_Type]
+GO
+
+ALTER TABLE [dbo].[Order]  WITH CHECK ADD  CONSTRAINT [FK_Order_User] FOREIGN KEY([UserId])
+REFERENCES [dbo].[User] ([UserId])
+GO
+
+ALTER TABLE [dbo].[Order] CHECK CONSTRAINT [FK_Order_User]
+GO
+
+
+
+/****** Object:  Table [dbo].[Status]    Script Date: 6/19/2018 9:25:35 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Status](
+	[OrderId] [int] NOT NULL,
+	[Status] [int] NOT NULL,
+	[RecordDate] [date] NOT NULL
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[Status]  WITH CHECK ADD  CONSTRAINT [FK_Status_Order] FOREIGN KEY([OrderId])
+REFERENCES [dbo].[Order] ([OrderId])
+GO
+
+ALTER TABLE [dbo].[Status] CHECK CONSTRAINT [FK_Status_Order]
+GO
+
+
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE spGetOrders
+AS
+BEGIN
+SELECT OrderId, Typeid, UserId, NotificationCode, PaypalGuid, RecordDate, Total FROM [dbo].[Order] WITH (NOLOCK)
+END
