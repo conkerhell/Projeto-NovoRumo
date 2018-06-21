e1c70e25ab1189c521babc5d4927571e3b8ab7ba
Victor Oliveira <fontiana@gmail.com>
2018-06-21 20:16:56 +0000

    Update Log.cs

--- NovoRumoProjeto.Utilities/LogManager/Log.cs
+++ NovoRumoProjeto.Utilities/LogManager/Log.cs
@@ -1,7 +1,28 @@
  
  namespace NovoRumoProjeto.Utilities.LogManager {
-  public class Log : ILogInterface
+  
+  public sealed class Log
   {
- 
+    private static Log instance = null;
+    private static readonly object padlock = new object();
+
+    Log()
+    {
+    }
+
+    public static Log Instance
+    {
+        get
+        {
+            lock (padlock)
+            {
+                if (instance == null)
+                {
+                    instance = new Log();
+                }
+                return instance;
+            }
+        }
+    }
   }
- }
+}
