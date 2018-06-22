a8e2eac1484a1900ad8db87b18e73f3f1c8f73d9
Victor Oliveira <fontiana@gmail.com>
2018-06-22 18:59:39 +0000

    Update LogModel.cs

--- NovoRumoProjeto.Utilities/LogManager/LogModel.cs
+++ NovoRumoProjeto.Utilities/LogManager/LogModel.cs
@@ -1,1 +1,20 @@
 
+namespace NovoRumoProjeto.Utilities.LogManager
+{    
+    public class LogModel
+    {
+        public LogModel()
+        {
+            
+        }   
+        
+        public Category Category { get; set; }
+    }
+
+    public enum Category
+    {
+        Info,
+        Warn,
+        Error        
+    }
+}
