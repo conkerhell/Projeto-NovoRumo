3dfedccfe38a97345411f0b9db0661894f5fbebd
Victor Oliveira <fontiana@gmail.com>
2018-06-30 19:59:26 -0300

    Fixing - terms and policy view

--- NovoRumoProjeto.DAL/Terms/TermsDAL.cs
+++ NovoRumoProjeto.DAL/Terms/TermsDAL.cs
@@ -12,8 +12,9 @@ namespace NovoRumoProjeto.DAL.Terms
     {
         public const string TITLE_COLUMN = "Title";
         public const string DESCRIPTION_COLUMN = "Description";
+        public const string TERM_ID_COLUMN = "TermID";
 
-        public const string GET_TERMS_PROC = "spGETTerms";
+        public const string GET_TERMS_PROC = "spGetTerms";
         public const string UPDATE_TERMS_PROC = "spUpdateTerms";
 
         public List<TermsEntity> Get()
@@ -36,6 +37,7 @@ namespace NovoRumoProjeto.DAL.Terms
                 {
                     if (result.Read())
                     {
+                        terms.TermID = Convert.ToInt32(result[TERM_ID_COLUMN]);
                         terms.Title = result[TITLE_COLUMN].ToString();
                         terms.Description = result[DESCRIPTION_COLUMN].ToString();
                     }
@@ -53,6 +55,7 @@ namespace NovoRumoProjeto.DAL.Terms
         public bool Update(TermsEntity entity)
         {
             return dataAccess.ExecuteNonQuery(UPDATE_TERMS_PROC,
+                dataAccess.ParameterFactory.Create(TERM_ID_COLUMN, DbType.Int32, entity.TermID, ParameterDirection.Input),
                  dataAccess.ParameterFactory.Create(TITLE_COLUMN, DbType.String, entity.Title, ParameterDirection.Input),
                  dataAccess.ParameterFactory.Create(DESCRIPTION_COLUMN, DbType.String, entity.Description, ParameterDirection.Input)) == 1;
         }
--- NovoRumoProjeto/Areas/Admin/Models/TermsViewModel.cs
+++ NovoRumoProjeto/Areas/Admin/Models/TermsViewModel.cs
@@ -4,16 +4,20 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace NovoRumoProjeto.Areas.Admin.Models
 {
     public class TermsViewModel
     {
+        public int TermId { get; set; }
+        [AllowHtml]
         [Required(ErrorMessageResourceType = typeof(LocalizedMessages),
            ErrorMessageResourceName = "PropertyValueRequired")]
         [Display(Name = "Titulo")]
         public string Title { get; set; }
 
+        [AllowHtml]
         [Required(ErrorMessageResourceType = typeof(LocalizedMessages),
             ErrorMessageResourceName = "PropertyValueRequired")]
         [Display(Name = "Descrição")]
