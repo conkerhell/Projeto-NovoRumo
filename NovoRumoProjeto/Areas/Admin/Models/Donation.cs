de7487ffc19e383477bce306b85c36478b65736a
Leonardo Ines <ConKer_hell@hotmail.com>
2018-07-18 10:40:59 -0300

    Cerating Model and Controller Donation.

---
+++ NovoRumoProjeto/Areas/Admin/Models/Donation.cs
@@ -0,0 +1,25 @@
+﻿using NovoRumoProjeto.Utilities;
+using NovoRumoProjeto.Utilities.Validation;
+using Resources;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NovoRumoProjeto.Areas.Admin.Models
+{
+    public class Donation
+    {
+        public int OrderId { get; set; }
+        public int TypeId { get; set; }
+        public int UserId { get; set; }
+        public string NotificationCode { get; set; }
+        public string PaypalGuid { get; set; }
+        public decimal Total { get; set; }
+        public DateTime? RecordDate { get; set; }
+
+
+    }
+}
