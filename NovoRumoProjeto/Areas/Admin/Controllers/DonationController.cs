dddbc0c7e9cb682dea1b2c520b8984f0e2ee45ce
Victor Oliveira <fontiana@gmail.com>
2018-07-26 13:55:44 +0000

    Ajustes no controller de donation

--- NovoRumoProjeto/Areas/Admin/Controllers/DonationController.cs
+++ NovoRumoProjeto/Areas/Admin/Controllers/DonationController.cs
@@ -36,77 +36,23 @@ namespace NovoRumoProjeto.Areas.Admin.Controllers
             }
             return View(model);
         }
-
-        // GET: Admin/Donation/Details/5
-        public ActionResult Details(int id)
+        
+        [HttpGet]
+        public ActionResult Details(int? id)
         {
             return View();
         }
 
-        // GET: Admin/Donation/Create
-        public ActionResult Create()
+        [HttpGet]
+        public ActionResult Edit(int? id)
         {
             return View();
         }
 
-        // POST: Admin/Donation/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Admin/Donation/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit()
         {
             return View();
-        }
-
-        // POST: Admin/Donation/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Admin/Donation/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Admin/Donation/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
