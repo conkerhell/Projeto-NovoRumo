8d2ff201b504fa279902ee1c43819e9f18a9db0c
Victor Oliveira <fontiana@gmail.com>
2018-09-17 00:09:29 -0300

    atualizando layout

--- Proc/About/INIT_ABOUT.sql
+++ Proc/About/INIT_ABOUT.sql
@@ -9,7 +9,7 @@ GO
 
 CREATE TABLE [dbo].[About](
 	[AboutID] [int] IDENTITY(1,1) NOT NULL,
-	[Title] [varchar](20) NOT NULL,
+	[Title] [varchar](50) NOT NULL,
 	[Description] [varchar](2000) NOT NULL,
 	[Filename] [varchar](100) NOT NULL,
 	[Data] [datetime] NOT NULL,
@@ -28,9 +28,7 @@ GO
 
 
 CREATE PROCEDURE [dbo].[spDeleteAbout]
-
 	@AbouID int
-	
 AS
 BEGIN
 DELETE FROM About WHERE AboutID = @AbouID
@@ -46,7 +44,7 @@ CREATE PROCEDURE [dbo].[spGetAbout]
 
 AS
 BEGIN
-	SELECT AboutID, Title, Description,Filename,data 
+SELECT AboutID, Title, Description, Filename, data 
   FROM About WITH (NOLOCK)
 END
 
@@ -60,7 +58,7 @@ CREATE PROCEDURE [dbo].[spGetAboutById]
 	@AboutID as int
 AS
 BEGIN
-	SELECT AboutID, Title, Description,Filename,data
+SELECT AboutID, Title, Description, Filename, data
   FROM About WITH (NOLOCK)
  WHERE AboutID = AboutID
 END
@@ -76,7 +74,7 @@ CREATE PROCEDURE [dbo].[spGetNewestAbout]
 AS
 BEGIN
 	
-	SELECT top 2 AboutID, Title, Description,Filename, Data   
+	SELECT AboutID, Title, Description, Description,Filename, Data   
 	 FROM About WITH (NOLOCK)
 ORDER BY Data 
 END
@@ -88,14 +86,13 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE PROCEDURE [dbo].[spInsertAbout]
-	@Title as varchar(20),
+	@Title as varchar(50),
 	@Description as varchar(2000),
 	@Filename as varchar(100),
 	@Data as datetime
-
-	AS
+AS
 BEGIN
-	INSERT About (Title,Description,Filename,Data) values(@Title,@Description,@Filename,@Data)
+	INSERT About (Title, Description, Filename, Data) values(@Title, @Description, @Filename, @Data)
 END
 
 SET ANSI_NULLS ON
@@ -106,20 +103,18 @@ GO
 
 CREATE PROCEDURE [dbo].[spUpdateAbout]
 	@AboutID as int,
-	@Title as varchar(20),
+	@Title as varchar(50),
 	@Description as varchar(2000),
 	@Filename as varchar(100),
 	@Data as datetime
 AS
 BEGIN
-	
-	UPDATE About 
+UPDATE About 
    SET Title = @Title,
 	   Description = @Description,
 	   Filename = @Filename,
 	   Data = @Data
  WHERE AboutID = @AboutID
-	
 END
 
 
